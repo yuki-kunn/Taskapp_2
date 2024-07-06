d043f9e9516dd47b3fa68e681c7518d489406c01
yuki-kunn <21e1248@andrew.ac.jp>
2024-07-06 18:55:39 +0900

    フロントの整形

---
+++ task_app2/src/constants/appdata.ts
@@ -0,0 +1,11 @@
+/**
+ * アプリ基本情報
+ * @type {Object}
+ */
+export const APP_DATA = {
+    APP_NAME: 'task_app2',
+    APP_URL: 'http://localhost:3000/',
+    APP_ICON: '/favicon.ico',
+    APP_DESCRIPTION: '説明',
+    LOCALE: 'ja',
+  }
