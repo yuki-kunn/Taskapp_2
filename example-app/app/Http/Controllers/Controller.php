f57b50e857e749cc7f91ab77ab6285d48cffb727
yuki-kunn <21e1248@andrew.ac.jp>
2024-07-06 20:01:24 +0900

    laravelちょっと修正

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class UserController extends Controller
 {
-    //
+    /**
+     * 指定ユーザーのプロフィールページを表示
+     */
+    public function show(string $id): Response
+    {
+        return Inertia::render('Users/Profile', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
 }
